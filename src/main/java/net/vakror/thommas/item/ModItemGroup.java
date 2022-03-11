11fdc97ad5ceabe4c6d2490ee698f4071272c312
vaylor27 <vaylor2027@gmail.com>
2022-03-10 21:52:12 -0500

    Fixed the "Mythril Blocks" tab to have the icon of the mythril block instead of the mythril ingot

--- src/main/java/net/vakror/thommas/item/ModItemGroup.java
+++ src/main/java/net/vakror/thommas/item/ModItemGroup.java
@@ -5,10 +5,11 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.vakror.thommas.Thommas;
+import net.vakror.thommas.block.ModBlocks;
 
 public class ModItemGroup {
     public static final ItemGroup MYTHRIL_ITEMS = FabricItemGroupBuilder.build(new Identifier(Thommas.MOD_ID, "mythril_items"),
             () -> new ItemStack(ModItems.MYTHRIL_INGOT));
     public static final ItemGroup MYTHRIL_BLOCKS = FabricItemGroupBuilder.build(new Identifier(Thommas.MOD_ID, "mythril_blocks"),
-            () -> new ItemStack(ModItems.MYTHRIL_INGOT));
+            () -> new ItemStack(ModBlocks.MYTHRIL_BLOCK));
 }

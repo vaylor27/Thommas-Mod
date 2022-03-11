8fc50e3426dcbad1a3fd8887205301b2eda9e2d1
vaylor27 <vaylor2027@gmail.com>
2022-03-10 20:51:06 -0500

    Added the Mythril Nugget and Raw Mythril

--- src/main/java/net/vakror/thommas/block/ModBlocks.java
+++ src/main/java/net/vakror/thommas/block/ModBlocks.java
@@ -5,7 +5,7 @@ import net.vakror.thommas.Thommas;
 public class ModBlocks {
 
     public static void registerModBlocks() {
-        Thommas.LOGGER.info("Registering ModBlocks for " + Thommas.MOD_ID + "!");
+        Thommas.LOGGER.info("Registering ModBlocks for " + Thommas.MOD_ID + "! ");
     }
 
 }
